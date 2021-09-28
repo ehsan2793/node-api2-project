80c45bc87a230d00a5b624c38e54fbfabd1e6350
ehsan2793 <ehsan2793@gmail.com>
2021-09-28 13:22:47 -0700

    posts can be update using put

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,7 +1,7 @@
 // implement your posts router here
 
 const router = require('express').Router();
-const { find, findById, insert } = require('./posts-model');
+const { find, findById, insert, update } = require('./posts-model');
 
 router.get('/', async (req, res) => {
     const allPost = await find();
@@ -41,6 +41,32 @@ router.post('/', async (req, res) => {
     } catch (error) {
         res.status(500).json({
             message: 'There was an error while saving the post to the database',
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const change = req.body;
+    const found = await findById(id);
+
+    try {
+        if (!found) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist',
+            });
+        } else if (!change.title || !change.contents) {
+            res.status(400).json({
+                message: 'Please provide title and contents for the post',
+            });
+        } else {
+            const updatedPost = await update(id, change);
+            const newlyUpdataedPost = await findById(updatedPost);
+            res.status(200).json(newlyUpdataedPost);
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'The post information could not be modified',
         });
     }
 });
