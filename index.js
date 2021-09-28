7822763bcac2c21ef7cd9305eb33a04d42201ec7
ehsan2793 <ehsan2793@gmail.com>
2021-09-28 11:48:23 -0700

    first commit making sure everything works

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server');
+
+const port = 3001
+
+server.listen(port, () => {
+    console.log(`\n *** server is listening on  http://localhost${port} *** \n`);
+})
