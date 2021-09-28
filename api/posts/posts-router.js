825f28d8470a9fb3c9f60c8c6c995ac023a6ed50
ehsan2793 <ehsan2793@gmail.com>
2021-09-28 12:13:37 -0700

    first 2 test are passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+
+const router = require('express').Router();
+
+
+
+
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express');
+const postRuter = require('./posts/posts-router');
 
 const server = express();
 server.use(express.json());
 
 
+server.use('/api/posts', postRuter)
 
+
+
+server.get('/', (req, res) => {
+    res.send('<h1>welcome to post  api type /api/posts to see the posts </h1>');
+})
 
 
 module.exports = server
